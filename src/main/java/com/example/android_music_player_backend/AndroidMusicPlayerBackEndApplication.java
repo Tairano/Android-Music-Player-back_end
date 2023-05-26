e9405945dbf01efdc76996c2a975fe1929da1fe3
Tairano <2023684800@qq.com>
2023-05-26 11:31:26 +0800

    build: 创建项目

---
+++ src/main/java/com/example/android_music_player_backend/AndroidMusicPlayerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.android_music_player_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidMusicPlayerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidMusicPlayerBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/android_music_player_backend/AndroidMusicPlayerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.android_music_player_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidMusicPlayerBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
